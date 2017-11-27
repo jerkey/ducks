f6b8fb31f32b3c52920caac5153fd9cda383e1e3
jerkey <jerkey@g-cipher.net>
2017-11-26 18:52:32 -0800

    log every act to /tmp/ducks.log

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from bottle import route, run, auth_basic, redirect, request
 
 import duckfavicon, serial
+import datetime
 
 arduino = serial.Serial(port='/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_A41323739353519050D0-if00', baudrate=9600)
 
 authfile = open('authfile','r') # first line of file is login, second is password
 lines=authfile.readlines()
+logfile = open('/tmp/ducks.log','wa') # open logfile for appending
 
 def check(user, pw):
   if user == lines[0].strip() and pw == lines[1].strip():
@@ -39,18 +41,25 @@ def index():
 @auth_basic(check)
 def ducks_open():
   arduino.write('1')
+  logfile.write(str(datetime.datetime.today())+' OPEN\n')
+  logfile.flush()
   return redirect('/?lastCommand=open')
 
 @route('/ducks/close')
 @auth_basic(check)
 def ducks_close():
   arduino.write('2')
+  logfile.write(str(datetime.datetime.today())+' CLOSE\n')
+  logfile.flush()
   return redirect('/?lastCommand=closed')
 
 @route('/ducks/stop')
 @auth_basic(check)
 def ducks_stop():
   arduino.write('0')
+  logfile.write(str(datetime.datetime.today())+' STOP\n')
+  logfile.flush()
   return redirect('/?lastCommand=stopped')
 
+logfile.write(str(datetime.datetime.today())+' server initialized\n')
 run(host='0.0.0.0', port=8100)
